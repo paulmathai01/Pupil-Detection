b6f8a12a74b1b5605239c3534dbaa9d1d88e61d9
akashsmaran <akashsmaran@gmail.com>
2019-01-29 23:14:04 +0530

    curses

--- pt_v12.py
+++ pt_v12.py
@@ -9,37 +9,7 @@ import numpy as np
 from skimage import img_as_bool
 from skimage import img_as_ubyte
 from skimage.morphology import skeletonize
-
-import sys,tty,termios
-class _Getch:
-    def __call__(self):
-            fd = sys.stdin.fileno()
-            old_settings = termios.tcgetattr(fd)
-            try:
-                tty.setraw(sys.stdin.fileno())
-                ch = sys.stdin.read(3)
-            finally:
-                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-            return ch
-
-def get():
-        inkey = _Getch()
-        """
-        while(1):
-                k=inkey()
-                if k!='':break
-        """
-        if k=='\x1b[A':
-                return "up"
-        elif k=='\x1b[B':
-                return "down"
-        elif k=='\x1b[C':
-                return "right"
-        elif k=='\x1b[D':
-                return "left"
-        else:
-                return "0"
-
+import curses
 
 # image resolution to be processed
 IMG_HEIGHT = 135
